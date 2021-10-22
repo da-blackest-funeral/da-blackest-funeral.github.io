f43984f9caa8f5f810ef2325bf9c820985bc5c5e
2020 <fyodor.kazaryan@bk.ru>
2021-10-22 08:47:45 +0300

    made right DOM

--- 5/main.js
+++ 5/main.js
@@ -1,30 +1,26 @@
-window.addEventListener("DOMContentLoaded", function (event) {
-    console.log("DOM fully loaded and parsed");
-});
-  
+window.addEventListener('DOMContentLoaded', function (event) {
+  document.getElementById("button").addEventListener("click", calculate)  
+})
+
+
 function calculate() {
-    let product = Number(document.getElementById("product").value);
-    let lot = Number(document.getElementById("lot").value);
   
-  
-let re = new RegExp(/^[0-9]+$/i);
-  if (product < 0 || lot < 0) {
-    alert("Числа не должны быть отрицательными");
-  }
-  else if (
-    !(
-        re.test(product) &&
-        re.test(lot)
-      )
-    ) {
+  var product = Number(document.getElementById("product").value);
+  var lot = Number(document.getElementById("lot").value);
+  var regExp = new RegExp(/^[0-9]+$/i);
+    if (product < 0 || lot < 0) {
+      alert("Числа не должны быть отрицательными");
+    }
+    else if (!(regExp.test(product) && regExp.test(lot))) {
       alert("Нужно писать число!");
     } else if (product == "") {
-      alert("Укажите цену первого товара");
+      alert("Укажите цену товара");
     } else if (lot == "") {
-      alert("Укажите количество единиц первого товара");
-    } else {
+      alert("Укажите количество единиц товара");
+    }
+   else {
       const cost = product * lot;
       document.getElementById("cost").innerHTML =
-        "Стоимость равна: " + cost + " Руб.";
-    }
-  }
+      "Стоимость равна: " + cost + " Руб.";
+   }
+}
